e87043d248a5fd42def3bb3648467b0164309037
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-01-09 14:38:03 +0100

    Create general container wrapped

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,30 @@
 </script>
 
 <template>
+  <div class="container-wrapped">
+    <header>
+      <!-- Import Header -->
 
+    </header>
+
+    <main>
+      <!-- Import Main -->
+
+    </main>
+
+    <footer>
+      <!-- Import Footer -->
+    </footer>
+
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss">
+@use './styles/general.scss' as *;
+
+div.container-wrapped {
+  width: 80%;
+  border: 1px solid red;
+  margin: 0 auto;
+}
 </style>
